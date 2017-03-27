2fe75e432660a8876460e4267fcf41803c110549
zxmeng <lucasmeng@Lucass-MacBook-Pro.local>
2017-03-27 20:42:32 +0800

    lalala

--- code/code/move_selector.py
+++ code/code/move_selector.py
@@ -17,58 +17,64 @@ def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 
-def move_selector_nn(fen, move):
-    feature_layer_1 = 32
-    feature_layer_2 = 128
+feature_layer_1 = 32
+feature_layer_2 = 128
 
-    feature_layer_soft = 256
-    feature_layer_final = 90
+feature_layer_soft = 256
+feature_layer_final = 90
 
-    tf.reset_default_graph()
-    sess = tf.InteractiveSession()
+tf.reset_default_graph()
+sess_m = tf.InteractiveSession()
 
-    # paras
-    # 32? to be determined
-    W_conv1 = weight_varible([3, 3, 9, feature_layer_1])
-    b_conv1 = bias_variable([feature_layer_1])
+# paras
+# 32? to be determined
+W_conv1_m = weight_varible([3, 3, 9, feature_layer_1])
+b_conv1_m = bias_variable([feature_layer_1])
 
-    # conv layer-1
-    x = tf.placeholder(tf.float32, [None, 810])
-    x_image = tf.reshape(x, [-1, 10, 9, 9])
+# conv layer-1
+x_m = tf.placeholder(tf.float32, [None, 810])
+x_image_m = tf.reshape(x_m, [-1, 10, 9, 9])
 
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_conv1_m = tf.nn.relu(conv2d(x_image_m, W_conv1_m) + b_conv1_m)
 
-    # conv layer-2
-    W_conv2 = weight_varible([3, 3, feature_layer_1, feature_layer_2])
-    b_conv2 = bias_variable([feature_layer_2])
+# conv layer-2
+W_conv2_m = weight_varible([3, 3, feature_layer_1, feature_layer_2])
+b_conv2_m = bias_variable([feature_layer_2])
 
-    h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2) + b_conv2)
+h_conv2_m = tf.nn.relu(conv2d(h_conv1_m, W_conv2_m) + b_conv2_m)
 
-    # full connection
-    W_fc1 = weight_varible([10 * 9 * feature_layer_2, feature_layer_soft])
-    b_fc1 = bias_variable([feature_layer_soft])
+# full connection
+W_fc1_m = weight_varible([10 * 9 * feature_layer_2, feature_layer_soft])
+b_fc1_m = bias_variable([feature_layer_soft])
 
-    h_conv2_flat = tf.reshape(h_conv2, [-1, 10 * 9 * feature_layer_2])
-    h_fc1 = tf.nn.relu(tf.matmul(h_conv2_flat, W_fc1) + b_fc1)
+h_conv2_m_flat = tf.reshape(h_conv2_m, [-1, 10 * 9 * feature_layer_2])
+h_fc1_m = tf.nn.relu(tf.matmul(h_conv2_m_flat, W_fc1_m) + b_fc1_m)
 
-    # output layer: softmax
-    W_fc2 = weight_varible([feature_layer_soft, feature_layer_final])
-    b_fc2 = bias_variable([feature_layer_final])
+# output layer: softmax
+W_fc2_m = weight_varible([feature_layer_soft, feature_layer_final])
+b_fc2_m = bias_variable([feature_layer_final])
 
-    y_conv = tf.nn.softmax(tf.matmul(h_fc1, W_fc2) + b_fc2)
+y_conv_m = tf.nn.softmax(tf.matmul(h_fc1_m, W_fc2_m) + b_fc2_m)
 
-    sess.run(tf.global_variables_initializer())
+sess_m.run(tf.global_variables_initializer())
 
+
+def init_move_selector():
     # create model saver
     saver = tf.train.Saver()
+    with sess_m.as_default():
+        saver.restore(sess_m, '../model/my-model-' + piece_type)
 
+def close_move_selector():
+    sess_m.close()
+
+def move_selector_nn(fen, move):
     output, piece_type = extract_features_dest(fen, move)
     print piece_type
     feature = np.zeros((1, 810))
     feature[0] = output[0:-1].split(',')
-    with sess.as_default():
-        saver.restore(sess, '../model/my-model-' + piece_type)
-        prediction = y_conv.eval(feed_dict={x: feature})
-        prediction = np.reshape(prediction, (10, 9)).astype(np.float32)
-    sess.close()
+    
+    prediction = y_conv_m.eval(feed_dict={x_m: feature})
+    prediction = np.reshape(prediction, (10, 9)).astype(np.float32)
+    
     return prediction
--- code/code/piece_selector.py
+++ code/code/piece_selector.py
@@ -26,55 +26,55 @@ feature_layer_soft = 256
 feature_layer_final = 90
 
 tf.reset_default_graph()
-sess = tf.InteractiveSession()
+sess_p = tf.InteractiveSession()
 
 # paras
 # 32? to be determined
-W_conv1 = weight_varible([3, 3, feature_chnl, feature_layer_1])
-b_conv1 = bias_variable([feature_layer_1])
+W_conv1_p = weight_varible([3, 3, feature_chnl, feature_layer_1])
+b_conv1_p = bias_variable([feature_layer_1])
 
 # conv layer-1
-x = tf.placeholder(tf.float32, [None, 9*10*feature_chnl])
-x_image = tf.reshape(x, [-1, 10, 9, feature_chnl])
+x_p = tf.placeholder(tf.float32, [None, 9*10*feature_chnl])
+x_image_p = tf.reshape(x, [-1, 10, 9, feature_chnl])
 
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_conv1_p = tf.nn.relu(conv2d(x_image_p, W_conv1_p) + b_conv1_p)
 
 # conv layer-2
-W_conv2 = weight_varible([3, 3, feature_layer_1, feature_layer_2])
-b_conv2 = bias_variable([feature_layer_2])
+W_conv2_p = weight_varible([3, 3, feature_layer_1, feature_layer_2])
+b_conv2_p = bias_variable([feature_layer_2])
 
-h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2) + b_conv2)
+h_conv2_p = tf.nn.relu(conv2d(h_conv1_p, W_conv2_p) + b_conv2_p)
 
 # full connection
-W_fc1 = weight_varible([10 * 9 * feature_layer_2, feature_layer_soft])
-b_fc1 = bias_variable([feature_layer_soft])
+W_fc1_p = weight_varible([10 * 9 * feature_layer_2, feature_layer_soft])
+b_fc1_p = bias_variable([feature_layer_soft])
 
-h_conv2_flat = tf.reshape(h_conv2, [-1, 10 * 9 * feature_layer_2])
-h_fc1 = tf.nn.relu(tf.matmul(h_conv2_flat, W_fc1) + b_fc1)
+h_conv2_flat_p = tf.reshape(h_conv2_p, [-1, 10 * 9 * feature_layer_2])
+h_fc1_p = tf.nn.relu(tf.matmul(h_conv2_flat_p, W_fc1_p) + b_fc1_p)
 
 # output layer: softmax
-W_fc2 = weight_varible([feature_layer_soft, feature_layer_final])
-b_fc2 = bias_variable([feature_layer_final])
+W_fc2_p = weight_varible([feature_layer_soft, feature_layer_final])
+b_fc2_p = bias_variable([feature_layer_final])
 
-y_conv = tf.nn.softmax(tf.matmul(h_fc1, W_fc2) + b_fc2)
+y_conv_p = tf.nn.softmax(tf.matmul(h_fc1_p, W_fc2_p) + b_fc2_p)
 
-sess.run(tf.global_variables_initializer())
+sess_p.run(tf.global_variables_initializer())
 
 def init_piece_selector():
     # create model saver
     saver = tf.train.Saver()
-    with sess.as_default():
+    with sess_p.as_default():
         saver.restore(sess, '../model/my-model-piece_selector')
     
 def close_piece_selector():
-    sess.close()
+    sess_p.close()
 
 def piece_selector_nn(fen):
     output = extract_features_piece(fen)
     feature = np.zeros((1, 720))
     feature[0] = output[0:-1].split(',')
 
-    prediction = y_conv.eval(feed_dict={x: feature})
+    prediction = y_conv_p.eval(feed_dict={x_p: feature})
     prediction = np.reshape(prediction, (10, 9)).astype(np.float32)
 
     return prediction
